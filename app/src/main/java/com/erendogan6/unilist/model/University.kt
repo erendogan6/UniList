901af3096b8dca78f422e947ab99b65b7b67fbf3
erendogan6 <erendogan6@hotmail.com>
2024-04-23 18:17:31 +0300

    feat(model): added University model to store detailed university information

---
+++ app/src/main/java/com/erendogan6/unilist/model/University.kt
@@ -0,0 +1,11 @@
+package com.erendogan6.unilist.model
+
+data class University(
+    val name: String,
+    val phone: String,
+    val fax: String,
+    val website: String,
+    val email: String,
+    val adress: String,
+    val rector: String
+)
