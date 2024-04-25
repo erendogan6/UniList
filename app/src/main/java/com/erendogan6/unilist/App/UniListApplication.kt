460d7ef5836b539370061d6972171efef552dfa1
erendogan6 <erendogan6@hotmail.com>
2024-04-26 00:10:51 +0300

    refactor(app): renamed package from App to app for naming consistency

--- app/src/main/java/com/erendogan6/unilist/App/UniListApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.erendogan6.unilist.App
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class UniListApplication : Application()
---
+++ app/src/main/java/com/erendogan6/unilist/app/UniListApplication.kt
@@ -0,0 +1,6 @@
+package com.erendogan6.unilist.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp class UniListApplication : Application()
