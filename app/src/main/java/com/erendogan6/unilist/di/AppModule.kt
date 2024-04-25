e01859c9a88c31e0cfd1c8572880985722a9ee67
erendogan6 <erendogan6@hotmail.com>
2024-04-26 00:03:02 +0300

    feat(network): set up OkHttpClient with timeouts in AppModule

--- app/src/main/java/com/erendogan6/unilist/di/AppModule.kt
+++ app/src/main/java/com/erendogan6/unilist/di/AppModule.kt
@@ -6,16 +6,27 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module @InstallIn(SingletonComponent::class) class AppModule {
 
-    private val BASE_URL = "https://storage.googleapis.com/invio-com/usg-challenge/universities-at-turkey/"
+    private val url = "https://storage.googleapis.com/invio-com/usg-challenge/universities-at-turkey/"
+
+    @Provides @Singleton fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            connectTimeout(30, TimeUnit.SECONDS)
+            readTimeout(30, TimeUnit.SECONDS)
+            writeTimeout(30, TimeUnit.SECONDS)
+        }.build()
+    }
+
 
     @Provides @Singleton fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        return Retrofit.Builder().baseUrl(url).client(provideOkHttpClient()).addConverterFactory(GsonConverterFactory.create()).build()
     }
 
     @Provides @Singleton fun provideApiService(retrofit: Retrofit): ApiService {
