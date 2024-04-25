fd7dd91acecf631df2de9f409f1714fcf16e7989
erendogan6 <erendogan6@hotmail.com>
2024-04-26 00:09:46 +0300

    feat(viewModel): refactored UniversityListViewModel to use MutableStateFlow for live data updates

--- app/src/main/java/com/erendogan6/unilist/viewmodel/UniversityListViewModel.kt
+++ app/src/main/java/com/erendogan6/unilist/viewmodel/UniversityListViewModel.kt
@@ -7,9 +7,24 @@ import androidx.paging.cachedIn
 import com.erendogan6.unilist.model.ProvinceWithExpansion
 import com.erendogan6.unilist.repository.UniversityRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel class UniversityListViewModel @Inject constructor(private val repository: UniversityRepository) : ViewModel() {
-    val provinces: Flow<PagingData<ProvinceWithExpansion>> = repository.getProvincesStream().cachedIn(viewModelScope)
+    private val _provincesFlow = MutableStateFlow<PagingData<ProvinceWithExpansion>?>(null)
+    val provincesFlow: MutableStateFlow<PagingData<ProvinceWithExpansion>?> = _provincesFlow
+
+    init {
+        loadInitialData()
+    }
+
+    private fun loadInitialData() {
+        viewModelScope.launch {
+            repository.getProvincesStream().cachedIn(viewModelScope).collectLatest {
+                _provincesFlow.value = it
+            }
+        }
+    }
 }
