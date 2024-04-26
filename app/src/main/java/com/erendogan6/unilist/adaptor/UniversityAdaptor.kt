3e1ad6f2238afb3abdf6d0a7141747ee018d738a
erendogan6 <erendogan6@hotmail.com>
2024-04-26 22:30:48 +0300

    refactor(adapter): streamlined UniversityAdapter with improved data binding and interaction handling

--- app/src/main/java/com/erendogan6/unilist/adaptor/UniversityAdaptor.kt
+++ app/src/main/java/com/erendogan6/unilist/adaptor/UniversityAdaptor.kt
@@ -57,54 +57,64 @@ class UniversityAdapter(private val onFavoriteClicked: (University) -> Unit) : R
         }
 
         fun bind(university: UniversityWithExpansion) {
+            with(binding) {
+                universityPhone.setOnClickListener { if (university.university.phone != "-") startDialer(university.university.phone) }
+                updateFavoriteIcon(university.university.isFavorite)
 
-            binding.universityPhone.setOnClickListener {
-                val phoneNumber = university.university.phone
-                if (phoneNumber != "-") {
-                    startDialer(phoneNumber)
+                favoriteIcon.setOnClickListener {
+                    toggleFavorite(university.university)
+                    updateFavoriteIcon(university.university.isFavorite)
+                }
+
+                universityWebsite.setOnClickListener {
+                    navigateToWebsite(university.university)
+                }
+
+                val detailsAreEmpty = checkDetailsEmpty(university.university)
+                expandIcon.visibility = if (detailsAreEmpty) View.INVISIBLE else View.VISIBLE
+
+                universityName.text = university.university.name
+                universityPhone.text = "Telefon: ${university.university.phone}"
+                universityFax.text = "Fax: ${university.university.fax}"
+                universityWebsite.text = "Website: ${university.university.website}"
+                universityAdress.text = "Adres: ${university.university.adress}"
+                universityRector.text = "Rektör: ${university.university.rector}"
+
+                updateExpandIcon(university.isExpanded)
+                detailsLayout.visibility = if (university.isExpanded) View.VISIBLE else View.GONE
+                universityAdress.setOnClickListener { openMap(university.university.adress) }
+                expandIcon.setOnClickListener {
+                    if (!detailsAreEmpty) {
+                        toggleDetailsVisibility(university)
+                        notifyItemChanged(bindingAdapterPosition)
+                    }
                 }
             }
+        }
 
-            val favoriteIconResId = if (university.university.isFavorite) R.drawable.favorite_icon else R.drawable.unfavorite_icon
-            binding.favoriteIcon.setImageResource(favoriteIconResId)
+        private fun updateFavoriteIcon(isFavorite: Boolean) {
+            binding.favoriteIcon.setImageResource(if (isFavorite) R.drawable.favorite_icon else R.drawable.unfavorite_icon)
+        }
 
-            binding.favoriteIcon.setOnClickListener {
-                val newFavoriteStatus = !university.university.isFavorite
-                onFavoriteClicked(university.university)
-                university.university.isFavorite = newFavoriteStatus
-                binding.favoriteIcon.setImageResource(if (newFavoriteStatus) R.drawable.favorite_icon else R.drawable.unfavorite_icon)
+        private fun toggleFavorite(university: University) {
+            val newFavoriteStatus = !university.isFavorite
+            onFavoriteClicked(university)
+            university.isFavorite = newFavoriteStatus
+        }
+
+        private fun navigateToWebsite(uni: University) {
+            if (uni.website != "-") {
+                val action = ListFragmentDirections.actionListFragmentToWebViewFragment(uni.website, uni.name)
+                itemView.findNavController().navigate(action)
             }
+        }
 
-            binding.universityWebsite.setOnClickListener {
-                val uni = university.university
-                val url = uni.website
-                val name = uni.name
-                if (url != "-") {
-                    val action = ListFragmentDirections.actionListFragmentToWebViewFragment(url, name)
-                    itemView.findNavController().navigate(action)
-                }
-            }
-
-            val detailsAreEmpty = with(university.university) {
-                phone == "-" && fax == "-" && website == "-" && adress == "-" && rector == "-"
-            }
-
-            binding.expandIcon.visibility = if (detailsAreEmpty) View.INVISIBLE else View.VISIBLE
-
-            binding.universityName.text = university.university.name
-            binding.universityPhone.text = "Telefon: ${university.university.phone}"
-            binding.universityFax.text = "Fax: ${university.university.fax}"
-            binding.universityWebsite.text = "Website: ${university.university.website}"
-            binding.universityAdress.text = "Adres: ${university.university.adress}"
-            binding.universityRector.text = "Rektör: ${university.university.rector}"
-            updateExpandIcon(university.isExpanded)
-            binding.detailsLayout.visibility = if (university.isExpanded) View.VISIBLE else View.GONE
-
-            binding.universityAdress.setOnClickListener {
-                val address = university.university.adress
+        private fun openMap(address: String) {
+            if (address != "-") {
                 val gmmIntentUri = Uri.parse("geo:0,0?q=$address")
-                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-                mapIntent.setPackage("com.google.android.apps.maps")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri).apply {
+                    setPackage("com.google.android.apps.maps")
+                }
 
                 if (mapIntent.resolveActivity(itemView.context.packageManager) != null) {
                     itemView.context.startActivity(mapIntent)
@@ -112,14 +122,14 @@ class UniversityAdapter(private val onFavoriteClicked: (University) -> Unit) : R
                     Toast.makeText(itemView.context, "Google Maps uygulaması yüklü değil", Toast.LENGTH_SHORT).show()
                 }
             }
-
-            binding.expandIcon.setOnClickListener {
-                if (detailsAreEmpty) return@setOnClickListener
-                university.isExpanded = !university.isExpanded
-                updateExpandIcon(university.isExpanded)
-                notifyItemChanged(bindingAdapterPosition)
-            }
         }
+
+        private fun toggleDetailsVisibility(university: UniversityWithExpansion) {
+            university.isExpanded = !university.isExpanded
+            updateExpandIcon(university.isExpanded)
+        }
+
+        private fun checkDetailsEmpty(uni: University): Boolean = listOf(uni.phone, uni.fax, uni.website, uni.adress, uni.rector).all { it == "-" }
 
         private fun updateExpandIcon(isExpanded: Boolean) {
             binding.expandIcon.setImageResource(if (isExpanded) R.drawable.collapse_icon else R.drawable.expand_icon)
