a933a4461c24de550a8ae8cd42aeb1979edabb6b
erendogan6 <erendogan6@hotmail.com>
2024-04-26 17:03:50 +0300

    feat(db): implemented UniversityDao with CRUD operations for favorite universities

---
+++ app/src/main/java/com/erendogan6/unilist/db/UniversityDao.kt
@@ -0,0 +1,15 @@
+package com.erendogan6.unilist.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.erendogan6.unilist.model.University
+
+@Dao interface UniversityDao {
+    @Query("SELECT * FROM favorite_universities") suspend fun getAllFavorites(): List<University>
+    @Insert(onConflict = OnConflictStrategy.REPLACE) suspend fun insertFavorite(university: University)
+    @Delete suspend fun deleteFavorite(university: University)
+    @Query("SELECT EXISTS(SELECT 1 FROM favorite_universities WHERE name = :universityName)") suspend fun isFavorite(universityName: String): Boolean
+}
