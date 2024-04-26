aa55e87b7798cae18077ba2a2ed92e6bbe464b69
erendogan6 <erendogan6@hotmail.com>
2024-04-26 17:01:51 +0300

    feat(db): annotated University model for Room with isFavorites support

--- app/src/main/java/com/erendogan6/unilist/model/University.kt
+++ app/src/main/java/com/erendogan6/unilist/model/University.kt
@@ -1,11 +1,7 @@
 package com.erendogan6.unilist.model
 
-data class University(
-    val name: String,
-    val phone: String,
-    val fax: String,
-    val website: String,
-    val email: String,
-    val adress: String,
-    val rector: String
-)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorite_universities") data class University(
+    @PrimaryKey val name: String, val phone: String, val fax: String, val website: String, val email: String, val adress: String, val rector: String, var isFavorite: Boolean = false)
